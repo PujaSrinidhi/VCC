70fb9aef32e8afaffa1bd7d1813a8d2d61418034
PujaSrinidhi <165243832+PujaSrinidhi@users.noreply.github.com>
2025-03-23 23:10:37 +0530

    app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def get_data():
     return jsonify({"message": "Hello from Flask API!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
